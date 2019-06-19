dc0910e1030d155ebe225b8fd606891016067e34
muletrain201906 <muletrain201906@users.noreply.github.com>
2019-06-19 10:18:51 +0100

    Process by reflection

--- src/main/java/com/mulesoft/training/MuleComponent.java
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -1,22 +1,30 @@
 package com.mulesoft.training;
 
-import java.util.Map;
+import java.util.*;
 
 public class MuleComponent {
 
-  public Map<String,String> processMap() {
-    // processMap implementation
-    return null;
-  }
+	public Map<String, String> processMap(Map<String, String> input) {
+		input.put("processedBy", "processMap");
+		return input;
+	}
 
-  public Map<String,String> processArray() {
-    // processArray implementation
-    return null;
-  }
+	public Map<String, String> processArray(List<String> input) {
+		Map<String, String> output = new HashMap<String,String>();
+		
+		output.put("message", input.get(0));
+		output.put("processedBy", "processArray");
+		
+		return output;
+	}
 
-  public Map<String,String> processString() {
-    // processString implementation
-    return null;
-  }
+	public Map<String, String> processString(String input) {
+		Map<String, String> output = new HashMap<String,String>();
+		
+		output.put("message", input);
+		output.put("processedBy", "processString");
+		
+		return output;
+	}
 
-} 
+}
